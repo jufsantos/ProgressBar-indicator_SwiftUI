39c0f0f0dca9af2d4510dace79a0f0e3ae56bcaa
julia <julia.fsantos2@gmail.com>
2020-04-02 16:37:58 -0300

    progressBar added

--- ProgressBar_SwiftUI/ProgressBar_SwiftUI/ContentView.swift
+++ ProgressBar_SwiftUI/ProgressBar_SwiftUI/ContentView.swift
@@ -14,9 +14,58 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            Text("ProgressBar/Indicator will go here")
+            ProgressBar(value: $sliderValue.wrappedValue,
+                    maxValue: self.maxValue,
+                    foregroundColor: .green)
+            .frame(height: 10)
+            .padding(30)
+            
+            Spacer()
+            
+            Slider(value: $sliderValue, in: 0...maxValue).padding(30)
             
         }
+    }
+}
+
+struct ProgressBar: View {
+    private let value: Double
+    private let maxValue: Double
+    private let backgroundEnabled: Bool
+    private let backgroundColor: Color
+    private let foregroundColor: Color
+    
+    init(value: Double, maxValue: Double, backgroundEnabled: Bool = true, backgroundColor: Color = Color(UIColor(red: 245/255,
+    green: 245/255,
+    blue: 245/255,
+    alpha: 1.0)), foregroundColor: Color = Color.black) {
+        self.value = value
+        self.maxValue = maxValue
+        self.backgroundEnabled = backgroundEnabled
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+    }
+    var body: some View{
+        
+        ZStack {
+            
+            GeometryReader { geometryReader in
+                
+                if self.backgroundEnabled {
+                    Capsule()
+                        .foregroundColor(self.backgroundColor)
+                }
+                
+                Capsule()
+                    .frame(width: self.progress(value: self.value, maxValue: self.maxValue, width: geometryReader.size.width))
+                    .foregroundColor(self.foregroundColor)
+                    .animation(.easeIn)
+            }
+        }
+    }
+    private func progress(value: Double, maxValue: Double, width: CGFloat) -> CGFloat {
+        let percentage = value/maxValue
+        return width * CGFloat(percentage)
     }
 }
 
