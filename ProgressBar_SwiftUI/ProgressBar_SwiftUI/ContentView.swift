00407b3560d1ce9fb7910d80f4bb244b4c344c94
julia <julia.fsantos2@gmail.com>
2020-04-02 17:28:21 -0300

    full tutorial project

--- ProgressBar_SwiftUI/ProgressBar_SwiftUI/ContentView.swift
+++ ProgressBar_SwiftUI/ProgressBar_SwiftUI/ContentView.swift
@@ -20,6 +20,15 @@ struct ContentView: View {
             .frame(height: 10)
             .padding(30)
             
+            ProgressCircle(value: $sliderValue.wrappedValue,
+                           maxValue: self.maxValue,
+                           style: .dotted,
+                           foregroundColor: .red,
+                           lineWidth: 10)
+                .frame(height: 100)
+            
+            
+            
             Spacer()
             
             Slider(value: $sliderValue, in: 0...maxValue).padding(30)
@@ -39,6 +48,7 @@ struct ProgressBar: View {
     green: 245/255,
     blue: 245/255,
     alpha: 1.0)), foregroundColor: Color = Color.black) {
+        
         self.value = value
         self.maxValue = maxValue
         self.backgroundEnabled = backgroundEnabled
@@ -66,6 +76,59 @@ struct ProgressBar: View {
     private func progress(value: Double, maxValue: Double, width: CGFloat) -> CGFloat {
         let percentage = value/maxValue
         return width * CGFloat(percentage)
+    }
+}
+
+struct ProgressCircle: View {
+    enum Stroke{
+        case line
+        case dotted
+        
+        func strokeStyle(lineWidth: CGFloat) -> StrokeStyle {
+            switch self {
+            case .line:
+                return StrokeStyle(lineWidth: lineWidth, lineCap: .round)
+            case .dotted:
+            return StrokeStyle(lineWidth: lineWidth, lineCap: .round, dash: [12])
+            }
+        }
+    }
+    private let value: Double
+    private let maxValue: Double
+    private let style: Stroke
+    private let backgroundEnabled: Bool
+    private let backgroundColor: Color
+    private let foregroundColor: Color
+    private let lineWidth: CGFloat
+    
+    init(value: Double, maxValue: Double, style: Stroke = .line, backgroundEnabled: Bool = true, backgroundColor: Color = Color(UIColor(red: 245/255,
+    green: 245/255,
+    blue: 245/255,
+    alpha: 1.0)), foregroundColor: Color = Color.black, lineWidth: CGFloat = 10) {
+        
+        self.value = value
+        self.maxValue = maxValue
+        self.style = style
+        self.backgroundEnabled = backgroundEnabled
+        self.backgroundColor = backgroundColor
+        self.foregroundColor = foregroundColor
+        self.lineWidth = lineWidth
+    }
+    var body: some View {
+        ZStack {
+            
+            if self.backgroundEnabled{
+                Circle()
+                    .stroke(lineWidth: self.lineWidth)
+                    .foregroundColor(self.backgroundColor)
+            }
+            Circle()
+                .trim(from: 0, to: CGFloat(self.value / self.maxValue))
+                .stroke(style: self.style.strokeStyle(lineWidth: self.lineWidth))
+                .foregroundColor(self.foregroundColor)
+                .rotationEffect(Angle.degrees(-90))
+                .animation(.easeIn)
+        }
     }
 }
 
